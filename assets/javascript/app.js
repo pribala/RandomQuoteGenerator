84f5082436d853d4ac9956dd273e7fb40b54a27e
Priya Balakrishnan <andu_pri@yahoo.com>
2017-10-11 15:35:48 -0400

    feat:Get quotes from API and tweet them.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,15 +1,41 @@
 $("document").ready(function(){
 	displayQuotes();
-});
-
-function displayQuotes() {
-  var queryURL = "https://api.forismatic.com/api/1.0/method=getQuote&key=&format=json&lang=en";
-  $.ajax({
-     url: queryURL,
-     method: "GET"
-  }).done(function(response) {
-           var results = response.data;
-           console.log(results);
+  $("#newQuote").on("click", function(e){
+      e.preventDefault();  
+      displayQuotes();
+   });
+  $("#tweet").on("click", function(e){
+    e.preventDefault();
+    var currentTweet = $("#quote").text()+" by "+$("#author").text();
+    if (currentTweet.length > 140) {
+      //this.popover('show');
+      $("#message").text("Tweet should be less than 140 chars");
+    }else {
+       var twtLink = 'http://twitter.com/home?status=' +encodeURIComponent(currentTweet);
+       window.open(twtLink,'_blank');
+     }
   });
+});
+function displayPopover() {
+   $("#tweet").popover({
+        title: 'Click Inside',
+        placement: 'bottom',        
+        template: '<div class="popover-all"><div class="popover-arrow"></div><div class="popover-inner"><h3 class="popover-title">Example</h3><div class="popover-content"><p> Clicks:0 </p></div></div></div>'
+    });
+}
+function displayQuotes() {
+  $.ajax({
+    url: "https://andruxnet-random-famous-quotes.p.mashape.com/", // The URL to the API. You can get this in the API page of the API you intend to consume
+    type: 'GET', // The HTTP Method, can be GET POST PUT DELETE etc
+    data: {}, // Additional parameters here
+    dataType: 'json',
+    success: function(data) { 
+     $("#quote").text(data.quote);  
+     $("#author").text("- "+data.author);                       },
+    error: function(err) { alert(err); },
+    beforeSend: function(xhr) {
+    xhr.setRequestHeader("X-Mashape-Authorization", "Q3JVg1p2HBmsh1XfJddwHoS5tPcgp14mD1JjsnAzjsW7UltL34"); // Enter here your Mashape key
+    }
+});
 }  
           
